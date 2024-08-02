36a8871dc3785cca4ca23e8b1696ed090e4d02ea
M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2024-08-02 15:41:11 +0530

    init commit

---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Models;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDbContext context;
+        public ProductController(ProductDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("AllProducts")]
+        public List<Product> GetAllProducts() => context.Products.ToList();
+
+        [HttpGet("AllProducts/{id}")]
+        public Product GetProductById(int id)
+        {
+            var found = context.Products.FirstOrDefault(p => p.productId == id);
+            if (found == null)
+                throw new Exception("Product not found!");
+
+            return found;
+        }
+
+        [HttpPost]
+        public string AddProduct(Product product)
+        {
+            product.productId = 0;
+            context.Products.Add(product);
+            context.SaveChanges();
+            return "Product added successfully!";
+        }
+
+        [HttpPut]
+        public string UpdateProduct(Product product)
+        {
+            var found = context.Products.FirstOrDefault(p => p.productId == product.productId);
+            if (found == null)
+                throw new Exception("Product not found!");
+            
+            found.productName = product.productName;
+            found.productPrice = product.productPrice;
+            context.SaveChanges();
+            return "Product updated successfully!";
+        }
+
+        [HttpDelete]
+        public string DeleteProduct(int id)
+        {
+            var found = context.Products.FirstOrDefault(p => p.productId == id);
+            if (found == null)
+                throw new Exception("Product not found!");
+
+            context.Products.Remove(found);
+            context.SaveChanges();
+            return "Product deleted successfully!";
+        }
+    }
+}
---
+++ ProductApi/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductApi.Models
+{
+    public class Product
+    {
+        public int productId { get; set; }
+        public string productName { get; set; }
+        public double productPrice { get; set; }
+    }
+
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
